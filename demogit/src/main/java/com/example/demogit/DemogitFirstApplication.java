dcfb29b5315047c13dc73c8dd97164a3e6e7d94d
vijaykumarhirapara <hiraparavijay@gmail.com>
2019-12-26 12:21:43 +0530

    final

---
+++ demogit/src/main/java/com/example/demogit/DemogitFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogitFirstApplication.class, args);
+	}
+
+}
